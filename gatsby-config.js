1bff0f55ad39eb3bda34763a6c48a0bee8dcc212
Tung Vo <tung.tan.vo@gmail.com>
2018-10-12 20:48:49 -0700

    update package.json

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Stellar by HTML5 UP",
-    author: "Hunter Chang",
-    description: "A Gatsby.js Starter based on Stellar by HTML5 UP"
+    title: "VoSquared Homepage",
+    author: "Tung Vo",
+    description: "Homepage for VoSquared"
   },
   plugins: [
     'gatsby-plugin-react-helmet',
